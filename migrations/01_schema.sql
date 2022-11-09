d9d401267ae1ac0b352bdf26ffebe6ff4c5fa5f0
Jodie Drover <jodiedrover@live.com>
2022-11-09 14:19:58 -0500

    draft schema

---
+++ migrations/01_schema.sql
@@ -0,0 +1,45 @@
+-- CREATE DATABASE midterm;
+-- \c midterm
+
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS items CASCADE;
+DROP TABLE IF EXISTS transactions CASCADE;
+DROP TABLE IF EXISTS categories CASCADE;
+
+-- Example:
+-- INSERT INTO users (name, email)
+-- VALUES ('Eva Stanley', 'sebastianguerra@ymail.com'),
+-- ** Skip id and password for seeds
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  password VARCHAR(255) DEFAULT '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u',
+  phone_number VARCHAR(50) NOT NULL,
+  street VARCHAR(50),
+  city VARCHAR(50),
+  provine VARCHAR(50) NOT NULL,
+  country VARCHAR(50) NOT NULL,
+  postal_code VARCHAR(6) NOT NULL,
+);
+
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(50) NOT NULL,
+  price_per_item INTEGER NOT NULL DEFAULT 0,
+  description TEXT,
+  photo_url VARCHAR(255) NOT NULL,
+  sold BOOLEAN NOT NULL DEFAULT FALSE,
+  condition SMALLINT NOT NULL DEFAULT 0
+);
+
+CREATE TABLE transactions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  seller_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  item_id INTEGER REFERENCES items(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0
+  purchase_date DATE NOT NULL
+);
