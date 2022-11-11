b1210cc2b17d88f7de99c808e895e3eff35df595
Edlyn Shih <edlyn0418@gmail.com>
2022-11-11 18:43:45 -0500

    Add css for new_item_form

--- routes/new-item.js
+++ routes/new-item.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const router  = express.Router();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'midterm',
+  port: '5432'
+});
 
 router.get('/items', (req, res) => {
-  res.render('new-item');
+  pool.query(`
+  SELECT category
+  FROM items
+  GROUP BY category
+  ORDER BY category`)
+  .then(result => {
+    const categories = [];
+
+    for (const category of result.rows) {
+      console.log(category)
+      categories.push(category.category)
+    }
+
+    console.log(categories);
+
+    const templateVars = {
+      categories: categories,
+      user: req.session.user_id
+    }
+    console.log(templateVars);
+    res.render('new-item', templateVars);
+  })
 });
 
 module.exports = router;
