9cfa8564fe60727756f0d00a2e71bd8316aefeaa
Sean Lee <lee.sean.cw@gmail.com>
2022-11-11 21:00:44 +0000

    add jquery for on hover dropdown menu

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,21 @@
 // Client facing scripts here
 
 $(document).ready(() => {
-  
+  $('#categories').hover(function () {
+      // over
+      $('#category-dropdown').css('display', 'flex');
+    }, function () {
+      // out
+      $('#category-dropdown').css('display', 'none');
+    }
+  );
+
+  $('#category-dropdown').hover(function () {
+    // over
+    $('#category-dropdown').css('display', 'flex');
+  }, function () {
+    // out
+    $('#category-dropdown').css('display', 'none');
+  }
+);
 });
