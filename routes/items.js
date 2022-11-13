afc98f58c55d43bcad85a24c6ca7671ce94276b5
Edlyn Shih <edlyn0418@gmail.com>
2022-11-13 18:06:33 -0500

    Add item page ejs.

--- routes/items.js
+++ routes/items.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../db/connection');
 const itemQueries = require('../db/queries/items');
 
+//GET route to show user create new item page
 router.get('/', (req, res) => {
   db.query(`
   SELECT category
@@ -27,6 +28,8 @@ router.get('/', (req, res) => {
     })
 });
 
+
+//POST route to update the items database and store the values in req.body
 router.post('/', (req, res) => {
   const userId = req.session.userId;
   console.log(req.body)
@@ -39,5 +42,37 @@ router.post('/', (req, res) => {
       res.send(e)
     });
 })
+
+//GET route to show users individual item page
+app.get("/items/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = req.body.longURL;
+  urlDatabase[id].longURL = longURL;
+  const userid = req.session.user_id;
+
+  if (!userid) {
+    return res.status(401).send("Please log in to see your tinyURLs.");
+  } else if (!urlDatabase[id]) {
+    return res.status(404).send("This tiny URL does not exist.");
+  } else if (userid !== urlDatabase[req.params.id].userID) {
+    return res.status(403).send("You are not authorized to access this tinyURL.");
+  } else {
+    res.redirect("/urls");
+  };
+
+});
+
+//POST route to let users update individual item info
+app.post("/items/:id", (req, res) => {
+
+
+});
+
+//POST route to let users update individual item info
+app.post("/items/:id/delete", (req, res) => {
+
+
+});
+
 
 module.exports = router;
