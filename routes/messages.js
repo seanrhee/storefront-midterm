b4e161761b67ece08637490caae74788cf89baaf
Jodie Drover <jodiedrover@live.com>
2022-11-16 21:37:12 -0500

    Removed unnecessary code

--- routes/messages.js
+++ routes/messages.js
@@ -1,45 +1,13 @@
 const express = require('express');
 const router = express.Router()
-const db = require('../db/connection');
-const messageQueries = require('../db/queries/messages');
+// const db = require('../db/connection');
+// const messageQueries = require('../db/queries/messages');
 
 
 router.get('/', (req, res) => {
-  const user = req.session.user_id;
+  const templateVars = { user: req.session.user_id }
 
-  messageQueries.getInboxForUser(user)
-    .then(messages => {
-      const ids = [];
-      const fullNames = [];
-      const photos = [];
-      const prices = [];
-      const sellerMessages = [];
-      const buyers = [];
-      const sellers = [];
-      const dates = [];
-
-      for (const info of messages) {
-        ids.push(info.message_id);
-        fullNames.push(info.full_name);
-        prices.push(info.price_per_item);
-        photos.push(info.photo_url);
-        sellerMessages.push(info.message);
-        buyers.push(info.buyer_id);
-        sellers.push(info.seller_id);
-        dates.push(info.date_sent);
-      }
-
-      const templateVars = {
-        user,
-        photos,
-        sellers,
-        prices,
-        fullNames,
-        sellerMessages
-      }
-
-      res.render('messages', templateVars);
-    })
+  res.render('messages', templateVars);
 });
 
 module.exports = router;
