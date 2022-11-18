df24496f244163debcc5cf7be4a3583c3e7b5614
Jodie Drover <jodiedrover@live.com>
2022-11-18 10:34:28 -0500

    css edits

--- public/scripts/messages.js
+++ public/scripts/messages.js
@@ -3,24 +3,24 @@ const createInboxElement = function (message) {
   const $inbox = $(`
 
   <div class="message-container">
-  <date class="date"> ${timeago.format(message.created_at)} </date>
 
-          <div class="contact-container">
-            <div class="user-name"> ${message.first_name} ${message.last_name} </div>
-            <div class="contact">
-              <div class="phone-number"> ${message.phone_number} </div>
-              <div class="email"> ${message.email} </div>
-              <div id="address"> ${message.city} ${message.province}</div>
-            </div>
-          </div>
+  <div class="contact-container">
+  <div class="contact">
+  <div class="phone-number"> ${message.phone_number} </div>
+  <div class="email"> ${message.email} </div>
+  <div id="address"> ${message.city} ${message.province}</div>
+  </div>
+  <div class="user-name"> ${message.first_name} ${message.last_name} </div>
+  </div>
 
-          <div class="response">
-            <article class="message">${message.message}</article>
-            <form class="reply" action="/compose-message/${message.creator_id}" method="GET">
-              <button type="submit" id="reply-button">Reply</button>
-            </form>
-      </div>
-</div>
+  <div class="response">
+  <article class="message">${message.message}</article>
+  <form class="reply" action="/compose-message/${message.creator_id}" method="GET">
+  <button type="submit" id="reply-button">Reply</button>
+  </form>
+  </div>
+  <date class="date"> Received ${timeago.format(message.created_at)} </date>
+  </div>
     </div>
     `);
   return $inbox;
