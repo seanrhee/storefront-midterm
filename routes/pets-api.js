93e9d467937e7e16bdce7fba4212999dabadbc2e
Jodie Drover <jodiedrover@live.com>
2023-03-01 14:34:53 -0500

    Create endpoint for pets by id

--- routes/pets-api.js
+++ routes/pets-api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const db = require('../db/connection');
+const petQueries = require('../db/queries/pets');
 
 router.get('/', (req, res) => {
   return db.query(`
@@ -16,6 +17,21 @@ router.get('/', (req, res) => {
         )
       );
     });
+});
+
+router.get('/:id', (req, res) => {
+  console.log("REQ PARAMS:", req.params)
+  const petId = req.params.id;
+
+  petQueries.getPetId(petId)
+  .then(pets => {
+    res.json({ pets });
+  })
+  .catch(err => {
+    res
+    .status(500)
+    .json({ error: err.message });
+  })
 });
 
 
